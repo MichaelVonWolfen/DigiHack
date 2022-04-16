0728c60ea7321776bbd7c4c22d941c0b27827a2b
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2022-04-17 00:32:59 +0300

    Finished navbar on mobile

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,10 +1,25 @@
 import "./navbar.sass";
 import logo from "../../assets/placeholder.png"
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 export default function() {
+    const mobileChangeState = () =>{
+        let mobileNav = document.getElementById("mobile_nav")
+        if (mobileNav)
+            mobileNav.classList.toggle("hidden")
+    }
     return (
         <div className={"navbarContainer"}>
             <a href="/"><img src={logo} alt="Site Logo"/></a>
-            <ul>
+            <ul className={"computer"}>
+                <li><a href="/">Home</a></li>
+                <li><a href="/lost">Lost a Pet</a></li>
+                <li><a href="/found">Found a pet</a></li>
+                <li><a href="/search">Search for a pet</a></li>
+                <MenuIcon className={"hamburgerMenu"} onClick={mobileChangeState}/>
+            </ul>
+            <ul className={"mobile hidden"} id={"mobile_nav"}>
+                <CloseIcon className={"closeIcon"} onClick={mobileChangeState}/>
                 <li><a href="/">Home</a></li>
                 <li><a href="/lost">Lost a Pet</a></li>
                 <li><a href="/found">Found a pet</a></li>
